8829847b5bf6f4926ce18b3f2efd208c71a7471a
Gayar78 <remi.thibault@outlook.fr>
2023-11-20 17:52:44 +0100

    start delivery app logic

--- src/model/DeliveryMan.java
+++ src/model/DeliveryMan.java
@@ -1,7 +1,8 @@
 package model;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class DeliveryMan {
     private int idDM;
@@ -37,29 +38,55 @@ public class DeliveryMan {
     }
 
     public ArrayList<GPS> bestDelivery(ArrayList<Pizza> pizzas, GPS position) {
-        // Créer une copie de la liste des pizzas pour ne pas modifier l'original
-        ArrayList<Pizza> pizzasCopy = new ArrayList<>(pizzas);
+        return bestDeliveryRecursive(new ArrayList<>(pizzas), position, 0);
+    }
 
-        // Calculer la distance entre la position actuelle du livreur et toutes les coordonnées GPS des pizzas
-        pizzasCopy.sort(Comparator.comparingDouble(pizza -> position.distanceKM(pizza.getGPS())));
-
-        // Sélectionner les pizzas en fonction de la distance et du temps restant
-        ArrayList<GPS> sortedGPS = new ArrayList<>();
-        double remainingTime = 0;
-
-        for (Pizza pizza : pizzasCopy) {
-            double distance = position.distanceKM(pizza.getGPS());
-            double deliveryTime = distance / AVGspeed * 60; // Temps de livraison en minutes
-            remainingTime += pizza.getRestTime();
-
-            // Vérifier si la pizza peut être livrée avant que son temps restant n'atteigne zéro
-            if (remainingTime <= deliveryTime) {
-                sortedGPS.add(pizza.getGPS());
-            } else {
-                break; // Sortir de la boucle si la pizza ne peut pas être livrée à temps
-            }
+    private ArrayList<GPS> bestDeliveryRecursive(List<Pizza> pizzas, GPS position, double elapsedTime) {
+        if (pizzas.isEmpty()) {
+            return new ArrayList<>(); // Aucune pizza à livrer
         }
 
-        return sortedGPS;
+        pizzas.sort(Comparator.comparingDouble(pizza -> position.distanceKM(pizza.getGPS())));
+
+        Pizza nextPizza = pizzas.get(0);
+        double distance = position.distanceKM(nextPizza.getGPS());
+        double deliveryTime = distance / AVGspeed * 60; // Temps de livraison en minutes
+        double remainingTime = nextPizza.getRestTime() - elapsedTime;
+
+        System.out.println("restTime: " + formatDouble(remainingTime, 2));
+        System.out.println("deliveryTime: " + formatDouble(deliveryTime, 2));
+
+        ArrayList<GPS> result = new ArrayList<>();
+
+        if (remainingTime > 0) {
+            // La pizza peut être livrée à temps
+            result.add(nextPizza.getGPS());
+
+            // Créer une nouvelle liste pour les pizzas restantes
+            List<Pizza> remainingPizzas = new ArrayList<>(pizzas.subList(1, pizzas.size()));
+
+            // Récursion pour traiter les pizzas restantes
+            ArrayList<GPS> remainingDelivery = bestDeliveryRecursive(remainingPizzas, nextPizza.getGPS(), elapsedTime + deliveryTime);
+            result.addAll(remainingDelivery);
+        } else {
+            // La pizza ne peut pas être livrée à temps, la traiter après les autres
+            result.addAll(bestDeliveryRecursive(pizzas.subList(1, pizzas.size()), position, elapsedTime));
+            result.add(nextPizza.getGPS());
+        }
+
+        return result;
+    }
+
+    public static String formatDouble(double number, int decimalPlaces) {
+        // Construire le motif en fonction du nombre de chiffres après la virgule
+        StringBuilder patternBuilder = new StringBuilder("#.");
+        for (int i = 0; i < decimalPlaces; i++) {
+            patternBuilder.append("#");
+        }
+        String pattern = patternBuilder.toString();
+        // Créer un objet DecimalFormat avec le motif spécifié
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        // Utiliser le format pour formater le nombre
+        return decimalFormat.format(number);
     }
 }
--- src/test/testMainDist.java
+++ src/test/testMainDist.java
@@ -6,21 +6,24 @@ import model.*;
 public class testMainDist {
     public static void main(String[] args) {
         // Create a list of delivery addresses.
-        GPS addressSamuel = new GPS(48.795385, 2.152272);
-        GPS addressPierre1 = new GPS(48.745385, 2.117144);
-        GPS addressPierre2 = new GPS(48.745385, 2.117145);
+        GPS Samuel = new GPS(48.795385, 2.152272);
+        GPS Pierre1 = new GPS(48.745385, 2.117144);
+        GPS Pierre2 = new GPS(48.745385, 2.117145);
+        GPS Buc = new GPS(48.774526, 2.122807);
 
-        double distancePierreSamuel = addressPierre1.distanceKM(addressSamuel);
-        System.out.println(distancePierreSamuel+"km between Pierre and Samuel (bird flight)");
+        //double distancePierreSamuel = Pierre1.distanceKM(Samuel);
+        //System.out.println(distancePierreSamuel+"km between Pierre and Samuel (bird flight)");
 
         // Create a list of pizzas.
         ArrayList<Pizza> pizzas = new ArrayList<>();
-        Pizza pizza1 = new Pizza(addressSamuel, 10);
-        Pizza pizza2 = new Pizza(addressPierre1, 20);
-        Pizza pizza3 = new Pizza(addressPierre2, 30);
+        Pizza pizza1 = new Pizza(Samuel, 10);
+        Pizza pizza2 = new Pizza(Pierre1, 5);
+        Pizza pizza3 = new Pizza(Pierre2, 4);
+        Pizza pizza4 = new Pizza(Buc, 3);
         pizzas.add(pizza1);
         pizzas.add(pizza2);
         pizzas.add(pizza3);
+        pizzas.add(pizza4);
 
         // Create a delivery man.
         DeliveryMan deliveryMan = new DeliveryMan(1, "John", "Doe", true);
