c695c2752ca2181be04574f3b2772b39bbe38db0
Gayar78 <remi.thibault@outlook.fr>
2023-12-06 15:37:59 +0100

    fastDelivery + restTimeDelivery

--- src/test/testMainDist.java
+++ src/test/testMainDist.java
@@ -25,15 +25,46 @@ public class testMainDist {
         pizzas.add(pizza3);
         pizzas.add(pizza4);
 
-        // Create a delivery man.
+
+        // Create a delivery man for the test of the bestDelivery method.
         DeliveryMan deliveryMan = new DeliveryMan(1, "John", "Doe", true);
-
-        // Get the best delivery addresses.
+        System.out.println(" ");
+        System.out.println("REDUCTIONLESS DELIVERY WITH FAST BACK");
+        // Get the best delivery patern in deliveryAddresses
         ArrayList<GPS> deliveredAddresses = deliveryMan.bestDelivery(pizzas, new GPS(48.739559173583984, 2.088090658187866));
-
         // Print the delivered addresses.
         for (GPS deliveredAddress : deliveredAddresses) {
             System.out.println(deliveredAddress);
         }
+        System.out.println(" ");
+        System.out.println("------------------------------------------------");
+
+
+        //Creat a delivery man for the test of the fast delivery method.
+        DeliveryMan deliveryMan2 = new DeliveryMan(2, "Jane", "Doe", true);
+        System.out.println(" ");
+        System.out.println("FAST DELIVERY WITH FAST BACK");
+        // Get the fastest delivery patern in deliveryAddresses2
+        ArrayList<GPS> deliveredAddresses2 = deliveryMan2.FastDelivery(pizzas, new GPS(48.739559173583984, 2.088090658187866));
+        // Print the delivered addresses.
+        for (GPS deliveredAddress : deliveredAddresses2) {
+            System.out.println(deliveredAddress);
+        }
+        System.out.println(" ");
+        System.out.println("------------------------------------------------");
+
+
+        //Creat a delivery man for the test of the rest_time delivery method.
+        DeliveryMan deliveryMan3 = new DeliveryMan(2, "Joe", "Doe", true);
+        System.out.println(" ");
+        System.out.println("FAST DELIVERY WITH FAST BACK");
+        // Get the best restTime delivery patern in deliveryAddresses3
+        ArrayList<GPS> deliveredAddresses3 = deliveryMan2.RestTimeDelivery(pizzas, new GPS(48.739559173583984, 2.088090658187866));
+        // Print the delivered addresses.
+        for (GPS deliveredAddress : deliveredAddresses3) {
+            System.out.println(deliveredAddress);
+        }
+        System.out.println(" ");
+        System.out.println("------------------------------------------------");
     }
 }
