d6f0684cc7518b449445e34929132f1bf771ab77
Fromont Boissel <pierrefromont@outlook.fr>
2023-12-09 23:40:39 +0100

    call openRoutesService API

--- src/main/java/com/pizzadelivery/model/GPS.java
+++ src/main/java/com/pizzadelivery/model/GPS.java
@@ -1,9 +1,17 @@
 package com.pizzadelivery.model;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Response;
+import com.pizzadelivery.config.ApiConfig;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 /**
  * A record that represents a geographic coordinate with a latitude and longitude.
@@ -45,29 +53,103 @@ public record GPS(double latitude, double longitude) {
         return earthRadius * c;
     }
 
-    public void timeToDeliver(GPS otherGPS) {
-        Client client = ClientBuilder.newClient();
-        Entity<String> payload = Entity.json("{\"coordinates\":[[8.681495,49.41461],[8.686507,49.41943],[8.687872,49.420318]]}");
-        Response response = client.target("https://api.openrouteservice.org/v2/directions/driving-car")
-                .request()
-                .header("Authorization", "5b3ce3597851110001cf62488087bfe5c98c461b8619f4d783400758")
-                .header("Accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8")
-                .header("Content-Type", "application/json; charset=utf-8")
-                .post(payload);
+    /**
+     * Calls the OpenRouteService API to get the driving directions and estimated duration
+     * between two GPS locations.
+     *
+     * @param source      The starting GPS location.
+     * @param destination The destination GPS location.
+     * @return A string containing the API response in JSON format.
+     * @throws Exception If an error occurs during the API call.
+     */
+    private static String callOpenRouteServiceApi(GPS source, GPS destination) throws Exception {
+        // Set up the URL and open a connection
+        URL url = new URL("https://api.openrouteservice.org/v2/directions/driving-car");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        System.out.println("status: " + response.getStatus());
-        System.out.println("headers: " + response.getHeaders());
-        System.out.println("body:" + response.readEntity(String.class));
+        // Set request method and headers
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Authorization", ApiConfig.OPENROUTE_API_KEY);
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+
+        // Create JSON payload with coordinates
+        JSONObject jsonPayload = new JSONObject();
+        jsonPayload.put("coordinates", new double[][]{
+                {source.longitude, source.latitude},
+                {destination.longitude, destination.latitude}
+        });
+
+        // Send request
+        try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = jsonPayload.toString().getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+
+        // Read and return response
+        StringBuilder response = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+            String responseLine;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+        }
+
+        return response.toString();
     }
 
+    /**
+     * Calculates the estimated time to travel between two GPS locations by car.
+     * The time is estimated using the OpenRouteService API.
+     *
+     * @param otherGPS The other GPS location to which the travel time is calculated.
+     * @return The estimated duration in seconds.
+     */
+    public double timeTravel(GPS otherGPS) {
+        try {
+            // Call the API and parse the response
+            String response = callOpenRouteServiceApi(this, otherGPS);
+            JSONObject jsonResponse = new JSONObject(response);
+            JSONArray routes = jsonResponse.getJSONArray("routes");
+
+            // Extract duration from the first route in the response
+            if (!routes.isEmpty()) {
+                JSONObject firstRoute = routes.getJSONObject(0);
+                JSONObject summary = firstRoute.getJSONObject("summary");
+                double duration = summary.getDouble("duration");
+
+                // Print and return the estimated duration
+                System.out.println("Estimated Duration (seconds): " + duration);
+                return duration;
+            }
+        } catch (Exception e) {
+            // Log exceptions
+            Logger logger = LoggerFactory.getLogger(GPS.class);
+            logger.error("Exception occurred", e);
+        }
+        return 0;
+    }
+
+    /**
+     * Returns a string representation of the GPS coordinate.
+     *
+     * @return A string representation of the GPS coordinate.
+     */
     @Override
     public String toString() {
         return String.format("GPS{latitude=%.6f, longitude=%.6f}", latitude, longitude);
     }
 
+    /**
+     * A main method that demonstrates the use of the GPS class.
+     *
+     * @param args Command-line arguments (unused).
+     */
     public static void main(String[] args) {
-        GPS gps1 = new GPS(49.41461, 8.681495);
-        GPS gps2 = new GPS(49.41943, 8.686507);
-        gps2.timeToDeliver(gps1);
+        GPS paris = new GPS(48.8566, 2.3522);
+        GPS versailles = new GPS(48.8049, 2.1204);
+        System.out.println(paris.timeTravel(versailles));
     }
 }
