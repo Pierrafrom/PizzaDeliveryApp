e631c4222a93cb20b542afbfe5d1c19b9d937ead
Fromont Boissel <pierrefromont@outlook.fr>
2024-01-09 15:20:00 +0100

    update SamuelAlgorithm

--- src/main/java/com/pizzadelivery/model/PierreAlgorithm.java
+++ src/main/java/com/pizzadelivery/model/PierreAlgorithm.java
@@ -6,39 +6,33 @@ import java.util.Collections;
 public class PierreAlgorithm {
 
     // -----------------------------------------------------------------------------------------------------------------
-    // BRUTE FORCE ALGORITHM FOR TIME CRITERIA
+    // Brute force algorithm with time criterion
     // -----------------------------------------------------------------------------------------------------------------
 
     public static ArrayList<Order> bruteForceTime(ArrayList<Order> orders) {
-        ArrayList<Order> bestOrderSequence = new ArrayList<>();
-        double bestTime = Double.MAX_VALUE;
+        ArrayList<Order> bestOrderSequence = new ArrayList<>(orders);
+        double[] bestTime = new double[] { Double.MAX_VALUE };
         permute(orders, 0, bestOrderSequence, bestTime);
         return bestOrderSequence;
     }
 
-    private static void permute(ArrayList<Order> orders, int k, ArrayList<Order> bestOrderSequence, double bestTime) {
+    private static void permute(ArrayList<Order> orders, int k, ArrayList<Order> bestOrderSequence, double[] bestTime) {
         for (int i = k; i < orders.size(); i++) {
             Collections.swap(orders, i, k);
             permute(orders, k + 1, bestOrderSequence, bestTime);
             Collections.swap(orders, k, i);
         }
         if (k == orders.size() - 1) {
-            double currentTime = calculateTotalDeliveryTime(orders);
-            if (currentTime < bestTime) {
-                bestTime = currentTime;
+            double currentTime = Order.totalDeliveryTime(orders);
+            if (currentTime < bestTime[0]) {
+                bestTime[0] = currentTime;
                 bestOrderSequence.clear();
                 bestOrderSequence.addAll(orders);
             }
         }
     }
 
-    private static double calculateTotalDeliveryTime(ArrayList<Order> orders) {
-        double totalTime = 0;
-        for (int i = 0; i < orders.size() - 1; i++) {
-            totalTime += Order.calculateDeliveryTime(orders.get(i), orders.get(i + 1));
-        }
-        return totalTime;
-    }
-
-
+    // -----------------------------------------------------------------------------------------------------------------
+    //
+    // -----------------------------------------------------------------------------------------------------------------
 }
