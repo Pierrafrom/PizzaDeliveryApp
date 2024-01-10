ab6cd02e741bf1a03104e370e29fa8aee4847a83
Gayar <132988567+Gayar78@users.noreply.github.com>
2024-01-10 15:07:33 +0100

    Update brute_force_distance

--- src/main/java/com/pizzadelivery/model/RemiAlgorithm.java
+++ src/main/java/com/pizzadelivery/model/RemiAlgorithm.java
@@ -11,13 +11,13 @@ public class RemiAlgorithm {
         }
         for(int i = 0; i<arr.size();i++){
             Order m = arr.get(i);
-            ArrayList<ArrayList<Order>> rem_arr = ArrayList<>();
-            for (int j = i + 1; j < arr.length; j++){
+            ArrayList<Order> rem_arr = new ArrayList<>();
+            for (int j = i + 1; j < arr.size(); j++){
                 rem_arr.add(arr.get(j);
             }
-            List<List<Integer>> combinations = generateCombinations(remArr, n - 1);
+            ArrayList<ArrayList<Order>> combinations = generate_combinations(remArr, n - 1);
 
-            for (List<Integer> p : combinations) {
+            for (ArrayList<Order> p : combinations) {
                 ArrayList<Order> newList = new ArrayList<>();
                 newList.add(m); // 'm' est un entier dans ce contexte
                 for(int n = 0; n<p.size();n++){
@@ -32,14 +32,14 @@ public class RemiAlgorithm {
         float min_distance = Float.POSITIVE_INFINITY;
         ArrayList<Order> best_combination = new ArrayList<>();
         ArrayList<ArrayList<Order> ALL_Orders = generate_combinations(orders, 5);
-        float total_distance = 0;
-        for(int i = 0; i<ALL_Order.size(); i++){
+        for(int i = 0; i<ALL_Orders.size(); i++){
+            float total_distance = 0;
             for(int j = 0; j<4;j++){
                 total_distance += calculateDeliveryDistance(ALL_Order.get(i).get(j), ALL_Order.get(i).get(j+1));
             }
             if(total_distance < min_distance){
                 min_distance = total_distance;
-                for(k = 0; k<ALL_Order.get(i).size(); k++){
+                for(int k = 0; k<ALL_Order.get(i).size(); k++){
                     best_combination.add(ALL_Order.get(i).get(k);
                 }
             }
