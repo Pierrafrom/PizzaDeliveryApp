1af79f6ff2a12953a1c92036bb41dc16aad59e68
Gayar <132988567+Gayar78@users.noreply.github.com>
2024-01-10 17:01:50 +0100

    Update RemiAlgorithm.java

--- src/main/java/com/pizzadelivery/model/RemiAlgorithm.java
+++ src/main/java/com/pizzadelivery/model/RemiAlgorithm.java
@@ -3,40 +3,35 @@ package com.pizzadelivery.model;
 import java.util.ArrayList;
 
 public class RemiAlgorithm {
-    public static ArrayList<ArrayList<Order>> generate_combinations(ArrayList<Order> arr, int n){
-        ArrayList<ArrayList<Order>> result = new ArrayList<>();
-        if(n == 0){
-            return new ArrayList<>();
+    public static ArrayList<ArrayList<Order>> generate_combinations(ArrayList<Order> orders) {
+        ArrayList<ArrayList<Order>> results = new ArrayList<>();
+        if (orders.isEmpty()) {
+            results.add(new ArrayList<>());
+            return results;
         }
-        for(int i = 0; i<arr.size();i++){
-            Order m = arr.get(i);
-            ArrayList<Order> rem_arr = new ArrayList<>();
-            for (int j = i + 1; j < arr.size(); j++){
-                rem_arr.add(arr.get(j));
-            }
-            ArrayList<ArrayList<Order>> combinations = generate_combinations(rem_arr, n - 1);
 
-            for (ArrayList<Order> p : combinations) {
-                ArrayList<Order> newList = new ArrayList<>();
-                newList.add(m);
-                newList.addAll(p);
-                result.add(newList);
+        for (int i = 0; i < orders.size(); i++) {
+            Order current = orders.remove(i);
+            ArrayList<ArrayList<Order>> permutations = generate_combinations(orders);
+            for (ArrayList<Order> permutation : permutations) {
+                permutation.add(0, current);
             }
+            results.addAll(permutations);
+            orders.add(i, current);
         }
-        return result;
+        return results;
     }
+
     public static ArrayList<Order> brute_force_distance(ArrayList<Order> orders){
         double min_distance = Double.POSITIVE_INFINITY;
         ArrayList<Order> best_combination = null;
-        ArrayList<ArrayList<Order>> ALL_Orders = generate_combinations(orders, 5);
-        for (ArrayList<Order> allOrder : ALL_Orders) {
+        ArrayList<ArrayList<Order>> ALL_Orders = generate_combinations(orders);
+        for (ArrayList<Order> order : ALL_Orders) {
             double total_distance = 0;
-            for (int j = 0; j < 4; j++) {
-                total_distance += Order.calculateDeliveryDistance(allOrder.get(j), allOrder.get(j + 1));
-            }
+            total_distance += Order.totalDeliveryDistance(order);
             if (total_distance < min_distance) {
                 min_distance = total_distance;
-                best_combination = new ArrayList<>(allOrder);
+                best_combination = new ArrayList<>(order);
             }
         }
         return best_combination;
