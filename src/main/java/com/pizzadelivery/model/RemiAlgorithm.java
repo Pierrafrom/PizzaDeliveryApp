ee32d5fd22878e4a43fce2fc9e683e5cf337d346
Gayar <132988567+Gayar78@users.noreply.github.com>
2024-01-10 15:22:48 +0100

    Update RemiAlgorithm.java

--- src/main/java/com/pizzadelivery/model/RemiAlgorithm.java
+++ src/main/java/com/pizzadelivery/model/RemiAlgorithm.java
@@ -3,47 +3,42 @@ package com.pizzadelivery.model;
 import java.util.ArrayList;
 
 public class RemiAlgorithm {
-    public static ArrayList<ArrayList<Order> generate_combinations(ArrayList<Order> arr, int n){
-        ArrayList<ArrayList<Order> result = new ArrayList<>();
+    public static ArrayList<ArrayList<Order>> generate_combinations(ArrayList<Order> arr, int n){
+        ArrayList<ArrayList<Order>> result = new ArrayList<>();
         if(n == 0){
-            ArrayList<ArrayList<Order> nothing = new ArrayList<ArrayList<Order>();
-            return nothing;
+            return new ArrayList<>();
         }
         for(int i = 0; i<arr.size();i++){
             Order m = arr.get(i);
             ArrayList<Order> rem_arr = new ArrayList<>();
             for (int j = i + 1; j < arr.size(); j++){
-                rem_arr.add(arr.get(j);
+                rem_arr.add(arr.get(j));
             }
-            ArrayList<ArrayList<Order>> combinations = generate_combinations(remArr, n - 1);
+            ArrayList<ArrayList<Order>> combinations = generate_combinations(rem_arr, n - 1);
 
             for (ArrayList<Order> p : combinations) {
                 ArrayList<Order> newList = new ArrayList<>();
                 newList.add(m); // 'm' est un entier dans ce contexte
-                for(int n = 0; n<p.size();n++){
-                    newList.add(p.get(n));
-                }
+                newList.addAll(p);
                 result.add(newList);
             }
         }
         return result;
     }
     public static ArrayList<Order> brute_force_distance(ArrayList<Order> orders){
-        float min_distance = Float.POSITIVE_INFINITY;
+        double min_distance = Float.POSITIVE_INFINITY;
         ArrayList<Order> best_combination = new ArrayList<>();
-        ArrayList<ArrayList<Order> ALL_Orders = generate_combinations(orders, 5);
-        for(int i = 0; i<ALL_Orders.size(); i++){
-            float total_distance = 0;
-            for(int j = 0; j<4;j++){
-                total_distance += calculateDeliveryDistance(ALL_Order.get(i).get(j), ALL_Order.get(i).get(j+1));
+        ArrayList<ArrayList<Order>> ALL_Orders = generate_combinations(orders, 5);
+        for (ArrayList<Order> allOrder : ALL_Orders) {
+            double total_distance = 0;
+            for (int j = 0; j < 4; j++) {
+                total_distance += Order.calculateDeliveryDistance(allOrder.get(j), allOrder.get(j + 1));
             }
-            if(total_distance < min_distance){
+            if (total_distance < min_distance) {
                 min_distance = total_distance;
-                for(int k = 0; k<ALL_Order.get(i).size(); k++){
-                    best_combination.add(ALL_Order.get(i).get(k);
-                }
+                best_combination.addAll(allOrder);
             }
         }
-        return totalDeliveryDistance(best_combination);
+        return best_combination;
     }
 }
